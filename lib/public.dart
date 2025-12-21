3003ff58ad577973862241c4e04ba01c252db2ba
Daniel Adeleke <danieladeleke05@gmail.com>
2025-12-21 23:40:42 +0100

    Wrote the password hashing logic

--- lib/public.dart
+++ lib/public.dart
@@ -1,8 +1,19 @@
 import 'dart:convert';
 
+import 'package:crypto/crypto.dart';
 import 'package:mongo_dart/mongo_dart.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
+
+String _hashPassword(String password, String salt) {
+  var key = utf8.encode(password);
+  var bytes = utf8.encode(salt);
+
+  var hmacSha256 = Hmac(sha256, key);
+  var digest = hmacSha256.convert(bytes);
+
+  return digest.toString();
+}
 
 void register(Router app, DbCollection users, DbCollection usersSessions) {
   app.post('/register', (Request request) async {
