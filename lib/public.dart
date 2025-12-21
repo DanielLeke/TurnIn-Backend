cab89c8c53bbf693918ce56643aad5b700eef5fd
Daniel Adeleke <danieladeleke05@gmail.com>
2025-12-21 23:39:17 +0100

    Writing out the logic for registering a user and added crypto package to the project

--- lib/public.dart
+++ lib/public.dart
@@ -7,7 +7,11 @@ import 'package:shelf_router/shelf_router.dart';
 void register(Router app, DbCollection users, DbCollection usersSessions) {
   app.post('/register', (Request request) async {
     final data = jsonDecode(await request.readAsString());
-    print(data);
+    var username = data['username'];
+    var password = data['password'];
+    var role = data['role'];
+
+    var document = {};
     return Response.ok('User registered');
   });
 }
