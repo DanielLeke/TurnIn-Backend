368ce0db567ac98037bfd987c8bebbe830f6cec8
Daniel Adeleke <danieladeleke05@gmail.com>
2025-12-22 22:13:52 +0100

    Wrote logic to decode encoded session

--- lib/student.dart
+++ lib/student.dart
@@ -1,7 +1,15 @@
+import 'dart:convert';
+
 import 'package:mongo_dart/mongo_dart.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 
 void submit(Router app, DbCollection usersSessions, DbCollection submissions) {
-  app.post('/submit', (Request request) async {});
+  app.post('/submit', (Request request) async {
+    final data = jsonDecode(await request.readAsString());
+    final session = data['session'];
+    final submission = data['submission'];
+
+    final decodedSession = json.decode(utf8.decode(base64.decode(session)));
+  });
 }
