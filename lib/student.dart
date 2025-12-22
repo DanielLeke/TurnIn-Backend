c2bcbf881e8b406712df2ed4b52eaf26bc951268
Daniel Adeleke <danieladeleke05@gmail.com>
2025-12-22 22:10:39 +0100

    Defined the endpoint for students to submit

--- lib/student.dart
+++ lib/student.dart
@@ -1,4 +1,7 @@
 import 'package:mongo_dart/mongo_dart.dart';
+import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 
-void submit(Router app, DbCollection usersSessions, DbCollection submissions) {}
+void submit(Router app, DbCollection usersSessions, DbCollection submissions) {
+  app.post('/submit', (Request request) async {});
+}
