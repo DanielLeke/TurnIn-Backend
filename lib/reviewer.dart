0dddaf39c3bd7db0a1b6ea3f4389baaa6a4a736f
Daniel Adeleke <danieladeleke05@gmail.com>
2025-12-23 01:09:17 +0100

    Wrote the user session checking logic

--- lib/reviewer.dart
+++ lib/reviewer.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:mongo_dart/mongo_dart.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
@@ -8,6 +10,33 @@ void getAllSubmissions(
   DbCollection submissions,
 ) {
   app.get('/submissions/all', (Request request) async {
-    
+    final data = jsonDecode(await request.readAsString());
+    final session = data['session'];
+
+    if (session == null) {
+      return Response.forbidden(
+        'No session provided',
+        headers: {'content-type': 'application/json'},
+      );
+    }
+
+    final decodedSession = json.decode(utf8.decode(base64.decode(session)));
+    final username = decodedSession['username'];
+    final sessionToken = decodedSession['sessionToken'];
+    final role = decodedSession['role'];
+
+    var userSession = await usersSessions.findOne(
+      where
+          .eq('username', username)
+          .eq('sessionToken', sessionToken)
+          .eq('role', role),
+    );
+
+    if (userSession == null) {
+      return Response.forbidden(
+        'Unauthorized user',
+        headers: {'content-type': 'application/json'},
+      );
+    }
   });
 }
