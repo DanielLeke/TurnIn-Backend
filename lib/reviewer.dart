b7b23b8bf4658fa75ef560453657c0350cd49f1e
Daniel Adeleke <danieladeleke05@gmail.com>
2025-12-23 02:06:15 +0100

    Changed the update input to mongodb

--- lib/reviewer.dart
+++ lib/reviewer.dart
@@ -96,9 +96,10 @@ void updateStatus(
         );
       }
 
-      await submissions.updateOne(where.eq('_id', ObjectId.fromHexString(id)), {
-        r'{$set: {status: Reviewed}}',
-      });
+      await submissions.updateOne(
+        where.eq('_id', ObjectId.fromHexString(id)),
+        {r'$set': {'status': 'Reviewed'}},
+      );
       return Response.ok(
         jsonEncode({'success': 'status updated'}),
         headers: {'content-type': 'application/json'},
